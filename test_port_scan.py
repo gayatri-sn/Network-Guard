43d24de16a97dfd20175ee340d91e2ef6cdc2dce
Richa Shanbhag <149705539+richashanbhag@users.noreply.github.com>
2025-11-09 19:42:55 +0530

    Update test_port_scan.py

--- test_port_scan.py
+++ test_port_scan.py
@@ -1,22 +1,19 @@
 # test_port_scan.py
-from scapy.all import sr1, IP, TCP, conf
+from scapy.all import srp1, IP, TCP, Ether, conf
 
-
-VICTIM_IP = "10.111.254.131"
+# --- CONFIG ---
+VICTIM_IP = "10.149.253.131"
+VICTIM_MAC = "10-68-38-74-A0-BF"
 ATTACKER_INTERFACE = "Wi-Fi"
+# --- END CONFIG ---
+
 conf.iface = ATTACKER_INTERFACE
-
-
-# 16 ports, which is > your threshold of 15
 PORTS_TO_SCAN = [21, 22, 23, 25, 53, 80, 110, 139, 443, 445,
                  1433, 1521, 3306, 3389, 5900, 8080]
 
-print(f"Running scan of {len(PORTS_TO_SCAN)} ports on {VICTIM_IP}...")
-
+print(f"Running L2 scan of {len(PORTS_TO_SCAN)} ports on {VICTIM_IP} ({VICTIM_MAC})...")
 for port in PORTS_TO_SCAN:
     print(f"Scanning port {port}...")
-    packet = IP(dst=VICTIM_IP) / TCP(dport=port, flags="S")
-    
-    sr1(packet, timeout=1, verbose=False) 
-
+    packet = Ether(dst=VICTIM_MAC.replace("-", ":")) / IP(dst=VICTIM_IP) / TCP(dport=port, flags="S")
+    srp1(packet, iface=ATTACKER_INTERFACE, timeout=1, verbose=False) 
 print("Scan complete. Check NIPS for 'Vertical Port Scan' alert.")
